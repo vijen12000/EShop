4c971d3c40cc94a9c3b93042519ee14e3363d4a8
Vijender Kumar <vijen12000@gmail.com>
2026-04-26 21:38:15 +0530

    Added Proudct Model, Create Produc Endpoint and Create Product Handler Classes

--- src/Services/Catalog/Catalog.API/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Catalog.API
-{
-    public class Product
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-    }
-}
